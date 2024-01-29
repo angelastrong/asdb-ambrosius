ef609ba72c2102742e5ef796e3842003666e9490
Angela Strong <3469924+angelastrong@users.noreply.github.com>
2024-01-28 19:44:32 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//const commandHandler = require('asdb-command-handler')
+const { Client, IntentsBitField } = require('discord.js')
+require('dotenv/config')
+
+const client = new Client({
+    intents: [IntentsBitField.Flags.Guilds],
+})
+
+client.on('ready', () => {
+    console.log('The bot is ready')
+})
+
+client.login(process.env.TOKEN)
